deed6d706fb53ad9045d43228a52d0b92330bcdf
Alexander Berryhill <amsb99@gmail.com>
2022-02-02 09:18:03 -0700

    Encapsulated clown

--- air/__main__.py
+++ air/__main__.py
@@ -15,6 +15,8 @@ balloon_list = []
 for i in range(10):
     balloon_list.append(clown.buy_balloon(random.randint(1,13)))
 
+print(clown)
+
 #while game is running
 first_balloon.update(current_time_ms())
 first_balloon.pop()
