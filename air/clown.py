576c78fcf4e50291a08f4f456434438b32b2e628
Alexander Berryhill <amsb99@gmail.com>
2022-02-28 09:47:50 -0700

    Added inheritence

--- air/clown.py
+++ air/clown.py
@@ -2,12 +2,16 @@ from person import Person
 from balloon import Balloon
 from air_tank import AirTank
 from wallet import Wallet
+from seller import Seller
 
 
-class Clown(Person):
+class Clown(Person, Seller):
     def __init__(self) -> None:
         super().__init__()
         self.__tank = AirTank("Helium")
+
+    def sell_item(self, cost: float):
+        return self.buy_balloon(cost)
 
     def buy_balloon(self, cost) -> Balloon:
         balloon = Balloon("Red")
